0f5a6c9e55cd650a875df04bda4d163fafa236fc
Triliam <taisbuenovidotto@gmail.com>
2025-04-29 10:15:29 -0300

    criacao classe Vendedor

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,15 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
 
         Department obj = new Department(1, "Livros");
-        System.out.println(obj);
+        Seller seller = new Seller(21,"Tais", "t@gmail.com", new Date(), 4.000, obj);
+        System.out.println(seller);
     }
 }
