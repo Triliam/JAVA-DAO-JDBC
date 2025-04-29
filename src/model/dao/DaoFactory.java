0cdeb2238f364cd53ac8d5658258eac1d343578e
Triliam <taisbuenovidotto@gmail.com>
2025-04-29 15:19:23 -0300

    implementacao departamento

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -2,11 +2,16 @@ package model.dao;
 
 import db.DB;
 import model.dao.impl.SellerDaoJDBC;
+import model.dao.impl.DepartmentDaoJDBC;
 
 public class DaoFactory {
 
     //dessa forma o programa não conhece a implementação, apenas a interface (SellerDao)
     public static SellerDao createSellerDao(){
         return new SellerDaoJDBC(DB.criaConnection());
+    }
+
+    public static DepartmentDao createDepartmentDao() {
+        return new DepartmentDaoJDBC(DB.criaConnection());
     }
 }
