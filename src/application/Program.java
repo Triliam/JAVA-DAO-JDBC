d720718f140e723a81719a04b81164ea6f584a84
Triliam <taisbuenovidotto@gmail.com>
2025-04-29 14:58:34 -0300

    criacao metodo delete

--- src/application/Program.java
+++ src/application/Program.java
@@ -7,6 +7,7 @@ import model.entities.Seller;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args) {
@@ -15,6 +16,8 @@ public class Program {
         Seller seller = new Seller(21,"Tais", "t@gmail.com", new Date(), 4.000, obj);
 
         SellerDao sellerDao = DaoFactory.createSellerDao(); //apenas a interface, podendo chamar qualquer implementação que esteja nela
+
+        Scanner scan = new Scanner(System.in);
 
         System.out.println("--- Test 1: seller findByid ---");
         Seller seller1 = sellerDao.findById(3);
@@ -44,5 +47,13 @@ public class Program {
         seller2.setName("Joice JJ");
         sellerDao.update(seller2);
         System.out.println("Update");
+
+        System.out.println("--- Test 6: seller delete ---");
+        System.out.println("Enter id for delete: ");
+        int id = scan.nextInt();
+        sellerDao.deleteById(id);
+        System.out.println("Deleted");
+
+        scan.close();
     }
 }
